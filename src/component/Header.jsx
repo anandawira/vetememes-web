0be3f60e086191d620661ef307ec32d25b482b33
Ananda Wiradharma <anandawira754@gmail.com>
2020-12-02 00:54:12 +0700

    remove boilerplates. add header

---
+++ src/component/Header.jsx
@@ -0,0 +1,31 @@
+import '../style/Header.scss';
+import logo from '../asset/vetememes.jpg';
+import { Link } from 'react-router-dom';
+
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div id="header">
+      <div id="logo">
+        <img src={logo} alt="logo" />
+        <p>
+          vete<span>memes</span>
+        </p>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">HOME</Link>
+          </li>
+          <li>
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
