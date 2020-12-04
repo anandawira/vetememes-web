483d95410edde17de56a0ee4ac1270a9181369b9
Ananda Wiradharma <anandawira754@gmail.com>
2020-12-04 12:46:35 +0700

    add name to profile card

--- src/component/ProfileCard.jsx
+++ src/component/ProfileCard.jsx
@@ -6,6 +6,9 @@ export default function ProfileCard(props) {
   return (
     <div className="profile-card">
       <img src={props.image} alt={props.name} />
+      <div className="info">
+        <p className="name">{props.name}</p>
+      </div>
     </div>
   );
 }
