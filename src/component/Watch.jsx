a94bc763c38b6b9fdcea16b3741732a25592eabb
Ananda Wiradharma <anandawira754@gmail.com>
2020-12-07 00:04:54 +0700

    add trailer to watch page

--- src/component/Watch.jsx
+++ src/component/Watch.jsx
@@ -3,19 +3,19 @@ import '../style/Watch.scss';
 import React, { useEffect } from 'react';
 
 export default function Watch(props) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      props.history.push('/');
-    }, 2000);
-    return () => {
-      clearTimeout(timer);
-    };
-  });
   return (
     <div id="watch">
-      This movie is not available yet.
-      <br />
-      Please comeback later.
+      <div id="trailer">
+        <iframe
+          title="trailer"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/ItZy2fuK55s"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
