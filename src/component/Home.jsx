c6fb59d31a40f297ddf6286c0135cddefc753aee
Ananda Wiradharma <anandawira754@gmail.com>
2020-12-06 21:47:46 +0700

    add mailchimp

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import '../style/Home.scss';
+import backdrop from '../asset/backdrop.webp';
+import { ReactComponent as MailIcon } from '../asset/mail_icon.svg';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 export default function Home() {
-  return <div id="home"></div>;
+  return (
+    <div id="home">
+      <img src={backdrop} alt="backdrop" />
+      <div id="subscribe-form">
+        <MailIcon id="mail-icon" />
+        <p>Subscribe to our newsletter</p>
+        <MailchimpSubscribe
+          id="mailchimp"
+          url={`https://site.us7.list-manage.com/subscribe/post?u=7ff2fa52ffbaa651e8c6e7c7d&amp;id=c903a2489b`}
+        />
+      </div>
+    </div>
+  );
 }
